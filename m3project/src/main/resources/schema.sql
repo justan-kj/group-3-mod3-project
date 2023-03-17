cf755ec4debc0a6df2da8fb40728abad88584dc3
Justin Tan <97202184+justan-kj@users.noreply.github.com>
2023-03-17 19:36:46 +0800

    Update schema.sql

--- m3project/src/main/resources/schema.sql
+++ m3project/src/main/resources/schema.sql
@@ -1,19 +1,42 @@
-INSERT INTO users (email, password, age, gender) VALUES
-('johndoe@example.com', 'password123', 25, 1),
-('janedoe@example.com', 'secret123', 35, 0),
-('bobsmith@example.com', 'qwerty123', 42, 1);
+create table users (
+    id int AUTO_INCREMENT NOT NULL,
+    email varchar(255) NOT NULL,
+    password char(50) NOT NULL,
+    age int,
+    gender int,
+    created_at timestamp default current_timestamp,
+    primary key (id)
+);
 
-INSERT INTO transport (description, price) VALUES
-('Flight from London to Paris', 120.00),
-('Train from Rome to Florence', 50.00),
-('Bus from Madrid to Barcelona', 25.00);
+create table transport (
+    id int AUTO_INCREMENT NOT NULL,
+    description varchar(255) NOT NULL,
+    price Decimal(8,2) NOT NULL,
+    created_at timestamp default current_timestamp,
+    primary key (id)
+);
 
-INSERT INTO accommodation (description, price) VALUES
-('Luxury hotel in Paris', 250.00),
-('Apartment in Rome', 100.00),
-('Hostel in Barcelona', 30.00);
+create table accommodation (
+    id int AUTO_INCREMENT NOT NULL,
+    description varchar(255) NOT NULL,
+    price Decimal(8,2) NOT NULL,
+    created_at timestamp default current_timestamp,
+    primary key (id)
+);
 
-INSERT INTO itineraries (user_id, transport_id, accommodation_id, destination_city, destination_country, destination_start_date, destination_end_date, budget) VALUES
-(1, 1, 1, 'Paris', 'France', '2023-05-01', '2023-05-05', 1000.00),
-(2, 2, 2, 'Rome', 'Italy', '2023-06-10', '2023-06-15', 1500.00),
-(3, 3, 3, 'Barcelona', 'Spain', '2023-07-20', '2023-07-25', 800.00);
+create table itineraries (
+    id int AUTO_INCREMENT NOT NULL,
+    user_id int NOT NULL,
+    transport_id int,
+    accommodation_id int,
+    destination_city varchar(255) NOT NULL,
+    destination_country varchar(255) NOT NULL,
+    destination_start_date varchar(255) NOT NULL,
+    destination_end_date varchar(255) NOT NULL,
+    budget Decimal(8,2),
+    created_at timestamp default current_timestamp,
+    primary key (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (transport_id) REFERENCES transport(id),
+    FOREIGN KEY (accommodation_id) REFERENCES accomodation(id)
+);
