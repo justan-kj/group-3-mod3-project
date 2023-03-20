bedd9e37518aa389f75e04db80fac20d6d53d253
Justin Tan <jtjtjt703@gmail.com>
2023-03-21 01:03:18 +0800

    Updated data for Destination and new name fields

--- m3project/src/main/resources/data.sql
+++ m3project/src/main/resources/data.sql
@@ -3,17 +3,24 @@ INSERT INTO users (email, password, age, gender) VALUES
 ('janedoe@example.com', 'secret123', 35, 0),
 ('bobsmith@example.com', 'qwerty123', 42, 1);
 
-INSERT INTO transport (description, price) VALUES
-('Flight from London to Paris', 120.00),
-('Train from Rome to Florence', 50.00),
-('Bus from Madrid to Barcelona', 25.00);
+INSERT INTO transport (name,description, price) VALUES
+('Singapore Airlines','Flight from London to Paris', 120.00),
+('EuroRail','Train from Rome to Florence', 50.00),
+('FlixBus','Bus from Madrid to Barcelona', 25.00);
 
-INSERT INTO accommodation (description, price) VALUES
-('Luxury hotel in Paris', 250.00),
-('Apartment in Rome', 100.00),
-('Hostel in Barcelona', 30.00);
+INSERT INTO accommodation (name,description, price) VALUES
+('Four Seasons','Luxury hotel in Paris', 2500.00),
+('Roma Saveilli','Apartment in Rome', 120.00),
+('One Paralelo','Hostel in Barcelona', 40.00);
 
-INSERT INTO itineraries (user_id, transport_id, accommodation_id, destination_city, destination_country, destination_start_date, destination_end_date, budget) VALUES
-(1, 1, 1, 'Paris', 'France', '2023-05-01', '2023-05-05', 1000.00),
-(2, 2, 2, 'Rome', 'Italy', '2023-06-10', '2023-06-15', 1500.00),
-(3, 3, 3, 'Barcelona', 'Spain', '2023-07-20', '2023-07-25', 800.00);
+INSERT INTO destinations (city, country, description)
+VALUES 
+  ('Paris', 'France', 'The City of Light and Love'),
+  ('Rome', 'Italy', 'The Eternal City'),
+  ('Barcelona', 'Spain', 'The City of Gaudi');
+
+INSERT INTO itineraries (user_id, transport_id, accommodation_id, destination_id, start_date, end_date, budget)
+VALUES 
+  (1, 1, 1, 1, '2023-05-01', '2023-05-05', 1000.00),
+  (2, 2, 2, 2, '2023-06-10', '2023-06-15', 1500.00),
+  (3, 3, 3, 3, '2023-07-20', '2023-07-25', 800.00);
