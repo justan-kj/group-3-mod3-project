0d393fde9bb7da6e065c3b9a79da19323981b3a6
Justin Tan <jtjtjt703@gmail.com>
2023-03-21 00:54:27 +0800

    Added Destination Repositories and Entities

---
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/entity/Destination.java
@@ -0,0 +1,75 @@
+package sg.edu.ntu.m3project.m3project.entity;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="destinations")
+public class Destination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "city")
+    String city;
+
+    @Column(name = "country")
+    String country;
+
+    @Column(name = "description")
+    String description;
+
+    @Column(name = "created_at", updatable = false)
+    Timestamp createdAt = new Timestamp(new Date().getTime());
+
+    //#region Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(final Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    //endregion
+}
