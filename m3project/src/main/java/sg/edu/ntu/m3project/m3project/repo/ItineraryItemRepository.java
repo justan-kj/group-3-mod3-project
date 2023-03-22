ac5a7a3f6f9a17226e5355ab5963ea7665c14904
Justin Tan <jtjtjt703@gmail.com>
2023-03-22 17:32:06 +0800

    added findAllByItinerary

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/repo/ItineraryItemRepository.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/repo/ItineraryItemRepository.java
@@ -6,11 +6,12 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
+import sg.edu.ntu.m3project.m3project.entity.Itinerary;
 import sg.edu.ntu.m3project.m3project.entity.ItineraryItem;
 
 @Repository
 public interface ItineraryItemRepository extends CrudRepository<ItineraryItem, Integer>{
   List<ItineraryItem> findAllByItineraryId(int itineraryId);
   Optional<ItineraryItem> findByitineraryId(int itineraryId);
+  List<ItineraryItem> findAllByItinerary (Itinerary itinerary);
 }
