ddca096d8ded2fc21020fb9ee2c6c37975471510
nbtai7777 <nbtai7777@gmail.com>
2023-03-24 21:55:08 +0800

    Revert "initial commit"
    This reverts commit 502167e559057b53d75c731dfe7735ae76182bd6.

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TransportController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TransportController.java
@@ -50,7 +50,6 @@ public class TransportController {
                 Transport t = currentTransport.get(); // Get the object - Transport
 
                 // Update the fetched product with description, price sent via Request Body
-                t.setName(transport.getName());
                 t.setDescription(transport.getDescription());
                 t.setPrice(transport.getPrice());
 
