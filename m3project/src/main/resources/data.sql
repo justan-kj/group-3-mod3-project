78a22fafee05d2f4becc536b6a622e5b8175fc97
Justin Tan <jtjtjt703@gmail.com>
2023-03-24 19:00:31 +0800

    fixed data

--- m3project/src/main/resources/data.sql
+++ m3project/src/main/resources/data.sql
@@ -13,12 +13,14 @@ VALUES ('New York', 'United States', 'The Big Apple'),
 INSERT INTO transport (name, description, price)
 VALUES ('Flight', 'This is air travel', 250.00),
 ('Train', 'This is railway travel', 100.00),
-('Car', 'This is a personal vehicle', 50.00);
+('Car', 'This is a personal vehicle', 50.00),
+('Walking', 'This is you', 0.00);
 
 INSERT INTO accommodation (name, description, price)
 VALUES ('Hotel', 'Luxury accommodations', 200.00),
 ('Hostel', 'Budget accommodations', 50.00),
-('Airbnb', 'Home-sharing accommodations', 100.00);
+('Airbnb', 'Home-sharing accommodations', 100.00),
+('Streets','Its free real estate', 0.00);
 
 INSERT INTO itineraries (name, description, user_id, start_date, end_date, budget)
 VALUES ('New York Trip', 'USA', 1, '2023-04-01', '2023-04-07', 1500.00),
@@ -33,7 +35,7 @@ VALUES ('New York Day 1', 'Departure flight to New York', 1, 1, 1, 1, '2023-04-0
 ('London Day 1', 'Arrival flight to London', 2, 1, 1, 2, '2023-05-01', '2023-05-02'),
 ('London Day 2', 'Hotel in central London', 2, 2, 3, 2, '2023-05-02', '2023-05-06'),
 ('Paris Day 1', 'Departure train to Paris', 3, 2, 4, 3, '2023-06-01', '2023-06-01'),
-('Paris Day 2', 'Cozy apartment in central Paris', 3, 1, 5, 3, '2023-06-02', '2023-06-04'),
+('Paris Day 2', 'Cozy apartment in central Paris', 3, 1, 4, 3, '2023-06-02', '2023-06-04'),
 ('Tokyo Day 1', 'Arrival flight to Tokyo', 4, 1, 3, 4, '2023-07-01', '2023-07-02'),
-('Tokyo Day 2', 'Ryokan in the countryside', 4, 1, 6, 4, '2023-07-03', '2023-07-04'),
-('Tokyo Day 3', 'City hotel in Shinjuku', 4, 2, 7, 4, '2023-07-05', '2023-07-06');
+('Tokyo Day 2', 'Ryokan in the countryside', 4, 1, 4, 4, '2023-07-03', '2023-07-04'),
+('Tokyo Day 3', 'City hotel in Shinjuku', 4, 2, 4, 4, '2023-07-05', '2023-07-06');
